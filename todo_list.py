91fe0c8a3eea4a5de161abcf401aa8d5f9b3d01f
shengshuyang <shengshuyangdlut@gmail.com>
2016-08-08 21:07:04 -0700

    todo list take 1

--- todo_list.py
+++ todo_list.py
@@ -4,16 +4,25 @@ import re
 from validation_util import *
 
 class ToDoHandler(webapp2.RequestHandler):
-    def write_form(self, form, item=""):
+
+    def create_item(self, item):
+         return "<tr><th>%(item)s</th></tr>"%{"item":item}
+
+    def write_form(self, form, items="", user_input=""):
         self.response.out.write(form % {
-          "item" : item})
+          "items" : items,
+          "user_input" : user_input})
 
     def get(self):
         content = get_html('/static/todo_list.html')
         self.write_form(content)
 
     def post(self):
-        self.redirect("welcome?username="+i_name)
+        item = self.request.get("user_input")
+        item = self.create_item(item)
+        content = get_html('/static/todo_list.html')
+        self.write_form(content, items = item)
+        #self.redirect("welcome?username="+i_name)
 
 app = webapp2.WSGIApplication([
     ('/todo_list', ToDoHandler)
