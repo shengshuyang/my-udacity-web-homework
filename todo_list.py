8a5dfe86299ec78b3a4693a61566b6772f4f9267
shengshuyang <shengshuyangdlut@gmail.com>
2016-08-08 23:09:13 -0700

    todo list take 1 finalized

--- todo_list.py
+++ todo_list.py
@@ -14,14 +14,30 @@ class ToDoHandler(webapp2.RequestHandler):
           "user_input" : user_input})
 
     def get(self):
+        items = self.request.get_all("item")
+        user_input = self.request.get("user_input")
         content = get_html('/static/todo_list.html')
-        self.write_form(content)
+        items_html = ""
+        for item in items:
+            items_html += self.create_item(item)
+        self.write_form(content%{"items":items_html, "user_input":user_input})
 
     def post(self):
-        item = self.request.get("user_input")
-        item = self.create_item(item)
-        content = get_html('/static/todo_list.html')
-        self.write_form(content, items = item)
+        items = self.request.get_all("item")
+        new_item = self.request.get("user_input")
+        items.append(new_item)
+        items_html = ""
+        for item in items:
+            items_html += self.create_item(item)
+
+        args = "item="+new_item
+        url = self.request.url
+        if "?" not in url:
+            url += "?"+args
+        else:
+            url += "&"+args
+
+        self.redirect(url.encode('ascii','ignore'))
         #self.redirect("welcome?username="+i_name)
 
 app = webapp2.WSGIApplication([
