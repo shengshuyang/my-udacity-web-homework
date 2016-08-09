da8ed51ccce1fb5b1d2f8123b94d0217ecd5cb3a
shengshuyang <shengshuyangdlut@gmail.com>
2016-08-09 12:38:32 -0700

    template escaping

--- handler.py
+++ handler.py
@@ -3,7 +3,9 @@ import jinja2
 import os
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(template_dir),
+    autoescape = True)
 
 class Handler(webapp2.RequestHandler):
 
